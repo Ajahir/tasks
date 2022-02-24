a6c2ab37897423f15e1979f7a7393814edbda6e9
Hp <Hp@DESKTOP-H5VIABE>
2022-02-24 21:30:02 +0530

    Add project files.

---
+++ tasks/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace tasks
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button reg;
+        private ImageView fb, ig;
+        private EditText name, email, user, pass;
+        private TextView log;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            reg = FindViewById<Button>(Resource.Id.button1);
+            fb = FindViewById<ImageView>(Resource.Id.imageView1);
+            ig = FindViewById<ImageView>(Resource.Id.imageView2);
+            name = FindViewById<EditText>(Resource.Id.editText1);
+            email = FindViewById<EditText>(Resource.Id.editText2);
+            user = FindViewById<EditText>(Resource.Id.editText3);
+            pass = FindViewById<EditText>(Resource.Id.editText4);
+            log = FindViewById<TextView>(Resource.Id.textView1);
+            reg.Click += Reg_Click;
+            fb.Click += Fb_Click;
+            ig.Click += Ig_Click;
+            log.Click += Log_Click;
+            
+        }
+
+        private void Log_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(log));
+        }
+
+        private void Ig_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "google sign in ", ToastLength.Short).Show();
+        }
+
+        private void Fb_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "facebook sign in ", ToastLength.Short).Show();
+        }
+
+        private void Reg_Click(object sender, System.EventArgs e)
+        {
+           
+           if (string.IsNullOrEmpty(name.Text) || string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(user.Text) || string.IsNullOrEmpty(pass.Text))
+                {
+                
+                
+                    Toast.MakeText(this, "enter full data", ToastLength.Short).Show();
+                
+                
+            }
+          
+           else
+            {
+                Toast.MakeText(this, "register successfully", ToastLength.Short).Show();
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ tasks/log.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tasks
+{
+    [Activity(Label = "log")]
+    public class log : Activity
+    {
+        private Button login;
+        private ImageView fb,ig;
+        private TextView user,pass,forgot;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login);
+            // Create your application here
+            login = FindViewById<Button>(Resource.Id.button1);
+            fb = FindViewById<ImageView>(Resource.Id.imageView1); 
+            ig = FindViewById<ImageView>(Resource.Id.imageView2);
+            forgot = FindViewById<TextView>(Resource.Id.forgot);
+            user = FindViewById<EditText>(Resource.Id.editText3);
+            pass=FindViewById<EditText>(Resource.Id.editText4);
+            forgot.Click += Forgot_Click;
+            fb.Click += Fb_Click;
+            ig.Click += Ig_Click;
+            login.Click += Login_Click;
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(user.Text) && string.IsNullOrEmpty(pass.Text))
+            {
+                Toast.MakeText(this, "enter full data", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "login  successfully", ToastLength.Short).Show();
+            }
+        }
+
+        private void Ig_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "google sign in ", ToastLength.Short).Show();
+        }
+
+        private void Fb_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "facebook sign in ", ToastLength.Short).Show();
+        }
+
+        private void Forgot_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this,"OTP send this number ",ToastLength.Short).Show();
+        }
+    }
+}
